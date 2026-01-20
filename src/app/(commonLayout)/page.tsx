ffb4505d172f76b784aa8212e31e77c9094db6b8
Fahim Muntashir <fahimasdf07@gmail.com>
2026-01-20 20:49:05 +0600

    Setting up postman and fetching dummy blog posts

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
 import { cookies } from "next/headers";
 
 export default async function Home() {
 
-  const {data,error}=await userService.getSession();
+const {data} = await blogService.getBlogPosts();
+
+console.log(data)
   return (
     <div>
       <Button variant="outline">Click Here</Button>
