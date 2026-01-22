32e0c948769fd4edf0129ba06f12618ae4515fc9
Fahim Muntashir <fahimasdf07@gmail.com>
2026-01-22 15:46:03 +0600

    blog fetch

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,17 +1,26 @@
+import BlogCard from "@/components/modules/homepage/BlogCard";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
+import { BlogPost } from "@/types/blog.type";
 import { cookies } from "next/headers";
 
 export default async function Home() {
 
-const {data} = await blogService.getBlogPosts();
+const {data} = await blogService.getBlogPosts({isFeatured:false,
+  search:"",
+},{
+  revalidate:10
+});
 
 console.log(data)
   return (
-    <div>
-      <Button variant="outline">Click Here</Button>
-    </div>
+    <div className="grid grid-cols-3 max-w-7xl mx-auto px-4 gap-5">
+{
+  data?.data.map((post:BlogPost)=>(
+    <BlogCard key={post.id} post={post}></BlogCard>
+  ))
+}    </div>
   );
 }
