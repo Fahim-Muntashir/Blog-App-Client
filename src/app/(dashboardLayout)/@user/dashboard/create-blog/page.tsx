43665497f1c0bcfcc9c6002acb2a4730e9c6d8d7
Fahim Muntashir <fahimasdf07@gmail.com>
2026-01-24 10:42:57 +0600

    Quick recap on post flow and fixing role issue

---
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -0,0 +1,5 @@
+const CreateBlogPage = () => {
+  return <div></div>;
+};
+
+export default CreateBlogPage;
--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -13,8 +13,10 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Roles } from "@/constants/roles";
+import { userService } from "@/services/user.service";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   admin,
   user,
 }: {
@@ -22,10 +24,13 @@ export default function DashboardLayout({
   admin: React.ReactNode;
   user: React.ReactNode;
 }) {
-  const userInfo = {
-    role: "admin",
-  };
+  // const userInfo = {
+  //   role: "admin",
+  // };
 
+  const { data } = await userService.getSession();
+  console.log(data);
+  const userInfo = data.user;
   return (
     <SidebarProvider>
       <AppSidebar user={userInfo} />
@@ -36,22 +41,9 @@ export default function DashboardLayout({
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
           />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          {userInfo.role === "admin" ? admin : user}
+          {userInfo.role === Roles.admin ? admin : user}
         </div>
       </SidebarInset>
     </SidebarProvider>
